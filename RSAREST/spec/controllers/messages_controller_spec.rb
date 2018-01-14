0bb870f4db88dd2c4c81889c85f2052f7b306271
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-01-15 00:26:59 +0200

    database.yml changed test base to be the same as development

--- RSAREST/spec/controllers/messages_controller_spec.rb
+++ RSAREST/spec/controllers/messages_controller_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe MessagesController do
 		id2 = JSON.parse(response.body)["id"]
 		get :show, as: :json, params: {id: 1, id2: id2}
 		message_encrypted = JSON.parse(response.body)["message"]
-		expect(message_encrypted.to_s).to eq "1815848.1000000.1481544"
+		expect(message_encrypted.to_s).to eq "259311.2088738.3731020"
 	end
 	it "encrypt and decrypt a message" do
-		post :decrypt_messages, as: :json, params: {id: 1, message:"1815848.1000000.1481544"}
+		post :decrypt_messages, as: :json, params: {id: 1, message:"259311.2088738.3731020"}
 		message_decrypted = JSON.parse(response.body)["message"]
 		expect(message_decrypted.to_s).to eq "zdr"
 	end
